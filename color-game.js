ee5f5375b7dd42098256fce39fc352a04a4bc093
chandu <chandrasr.palle@gmail.com>
2018-04-12 10:18:27 +0200

    added reset button and random colors functionality

--- color-game.js
+++ color-game.js
@@ -1,15 +1,26 @@
 var squares = document.querySelectorAll(".square");
-var colors = [
-                "rgb(255, 0, 0)",
-                "rgb(23, 244, 23)",
-                "rgb(222, 255, 0)",
-                "rgb(0, 2, 253)",
-                "rgb(200, 34, 223)",
-                "rgb(0, 234, 223)"
-            ];
+var colors = generateRandomColors(6);
 var pickedColor = pickColorFunction();
-document.querySelector("#pickedColor").textContent = pickedColor;
+var colorDisplay = document.querySelector("#pickedColor");
 var message = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyLevel");
+var hardBtn = document.querySelector("#hardLevel");
+colorDisplay.textContent = pickedColor;
+
+resetBtn.addEventListener('click',function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColorFunction();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        //loop through all the colors
+        squares[i].style.backgroundColor = colors[i];
+    }    
+    message.textContent = "TRY NOW!!";
+    h1.style.backgroundColor = "beige";
+});
+
 
 for(var i = 0; i < squares.length; i++){
     //loop through all the colors
@@ -18,12 +29,14 @@ for(var i = 0; i < squares.length; i++){
     //add event listeners for each square
     squares[i].addEventListener('click',function(){
         if(pickedColor == this.style.backgroundColor){
-            message.textContent = "correct";
+            message.textContent = "CORRECT";
+            resetBtn.textContent = "PLAY AGAIN!!"
             changeColor(pickedColor);
+            h1.style.backgroundColor = pickedColor;
         }
         else{
         this.style.backgroundColor = "black";
-        message.textContent = "Try again!";
+        message.textContent = "TRY AGAIN!";
         }
     });
 
@@ -37,4 +50,25 @@ function changeColor(color){
 function pickColorFunction(){
     var randomNum = Math.floor(Math.random() * colors.length);
     return colors[randomNum];
+}
+function generateRandomColors(num){
+    //define an empty array
+    colorArray = [];
+    // fill it with number of colors
+    for( var i = 0; i < num; i++){
+        colorArray[i] = randomColor();
+    }
+
+    return colorArray;
+
+}
+function randomColor(){
+    //red
+    var r = Math.floor(Math.random() * 256);
+    //blue
+    var g = Math.floor(Math.random() * 256);
+    //green
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb("+r+", "+g+", "+b+")";
 }
